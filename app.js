f80a959097556eb1d8c93c2b5ae703a4dfb945d1
wojtko <j.w2001@onet.pl>
2023-03-05 18:57:13 +0100

    Add some layouts and modify app.js

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ app.use(express.static("public"));
 const port = 3000;
 
 let items = ["Read the book", "Code", "Buy food"];
+let workItems = [];
 
+// home page
 
 app.get('/', (req, res) => {
     const today = new Date();
@@ -28,19 +30,42 @@ app.get('/', (req, res) => {
     const day = today.toLocaleDateString("en-US", options);
 
     res.render('list', {
-        kindOfDay: day,
+        listTitle: day,
         newListItems: items
     });
 
 });
 
 app.post('/', (req, res) => {
-    const item = req.body.newItem;
-    items.push(item);
 
-    res.redirect('/');
+    let item = req.body.newItem;
+
+    if (req.body.list === "Work List") {
+        workItems.push(item);
+        res.redirect('/work');
+    } else {
+        items.push(item);
+        res.redirect('/');
+    }
+
 });
 
+// Work page
+
+app.get('/work', (req, res) => {
+    res.render("list", {
+        listTitle: "Work List",
+        newListItems: workItems
+    });
+});
+
+// about page
+
+app.get('/about', (req, res) => {
+    res.render("about");
+});
+
+// listening
 
 app.listen(port, () => {
     console.log(`Server listening on ${port}...`);
