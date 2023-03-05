481937b8b6d20451845594daa393d9482730dd6b
wojtko <j.w2001@onet.pl>
2023-03-05 11:14:19 +0100

    Add ejs to project

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use('view engine', 'ejs');
+
 const port = 3000;
 
 app.get('/', (req, res) => {
     var today = new Date();
+    var currentDay = today.getDay();
+
+    if (currentDay === 6 || currentDay === 0) {
+        res.send("Yay it's the weekend!");
+    } else {
+        res.sendFile(__dirname + '/index.html');
+    }
 });
 
 
